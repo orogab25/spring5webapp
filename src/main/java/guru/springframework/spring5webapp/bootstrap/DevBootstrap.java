ff4b4bb0b5bd84a5ed71908a0708fc7848262784
orogab25 <orogab25@gmail.com>
2019-04-09 14:39:05 +0200

    Added data

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,17 +29,22 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     }
 
     private void initData(){
-        //Eric
+
+        //Publishers
+        Publisher publisher=new Publisher();
+        publisher.setName("qwe");
+        publisherRepository.save(publisher);
+
+        //Authors
         Author eric=new Author("Eric","Butcher");
-        Book asd=new Book("joska","12m","mmm");
+        Book asd=new Book("joska","12m",publisher);
         eric.getBooks().add(asd);
         asd.getAuthors().add(eric);
         authorRepository.save(eric);
         bookRepository.save(asd);
 
-        //Pista
         Author pista=new Author("Pista","Butcher");
-        Book bsd=new Book("hello","20u","lll");
+        Book bsd=new Book("hello","20u",publisher);
         pista.getBooks().add(bsd);
         authorRepository.save(pista);
         bookRepository.save(bsd);
